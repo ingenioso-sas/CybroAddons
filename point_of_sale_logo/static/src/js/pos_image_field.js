a221b6f541bbe5c2814035f9e26007d97b8bb087
jymontero <jmontero@ingenioso.co>
2025-07-15 23:36:05 +0000

    [UPD] Ajustes en métodos JavaScript,Python y XML según recomendaciones de SonarQube.

--- point_of_sale_logo/static/src/js/pos_image_field.js
+++ point_of_sale_logo/static/src/js/pos_image_field.js
@@ -1,29 +1,11 @@
 odoo.define("point_of_sale_logo.image", function (require) {
     "use strict";
-    var PosBaseWidget = require('point_of_sale.chrome');
-    var screens = require('point_of_sale.screens');
-    var core = require('web.core');
-
-    /*var QWeb = core.qweb;
-    console.log("PosBaseWidget", PosBaseWidget)
-    screens.ReceiptScreenWidget.include({
-        render_receipt: function () {
-            this._super(this);
-            var order = this.pos.get_order()
-            this.$('.pos-receipt-container').html(QWeb.render('OrderReceipt',{
-                    widget:this,
-                    a2 : window.location.origin + '/web/image?model=pos.config&field=image&id='+this.pos.config.id,
-                    order: order,
-                    receipt: order.export_for_printing(),
-                    orderlines: order.get_orderlines(),
-                    paymentlines: order.get_paymentlines(),
-                }));
-        },
-    });*/
+    const PosBaseWidget = require('point_of_sale.chrome');
+    
     PosBaseWidget.Chrome.include({
         renderElement:function () {
 
-            var self = this;
+            const self = this;
             console.log("self:", self)
 
             if(self.pos.config){
